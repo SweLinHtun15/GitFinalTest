c433b78ee1b197579b51481e42497da2f11eebf2
SweLinHtun15 <swelinhtun15@gmail.com>
2019-12-21 20:12:48 +0630

    Variable

---
+++ Variable.py
@@ -0,0 +1,84 @@
+#21.12.19
+
+>>> 2 * 2
+4
+>>> 50 - 5 * 6
+20
+>>> (50 - 5 * 6) / 4
+5.0
+>>> round(5.0)
+5
+>>> 5.123456
+5.123456
+>>> round(5.123456)
+5
+>>> round(5.123456,2)
+5.12
+>>> 17 / 3
+5.666666666666667
+>>> 17 // 3
+5
+>>> 18 // 4
+4
+>>> 20 // 5
+4
+>>> 17 % 3
+2
+>>> 17 * 4 % 3
+2
+a = 1
+a(variable) = (assign) 1 (value)
+ width = 20
+ height = 5 * 9
+ vol = width * height
+ vol
+
+ sale = 30000
+ tax = 5 / 100
+ total_tax = sale * tax
+ total_tax
+ total_sale = sale + total_tax
+ total_sale
+
+ >>> 'spam'
+'spam'
+>>> 'doesn\'t'
+"doesn't"
+>>> "doesn't"
+"doesn't"
+>>> '"Yes", they said.'
+'"Yes", they said.'
+>>> "\"Yes\", they said."
+'"Yes", they said.'
+>>> '"Isn\'t," they said.'
+'"Isn\'t," they said.'
+
+# \n = new line
+
+>>> s = 'First line.\nSecond Line.'
+>>> print(s)
+First line.
+Second Line.
+
+print("""\
+Usage: thingy
+		-a
+		-b
+		-c
+""")
+
+"""...""" or '''...''' 
+
+3 * 'A'
+2 * 'BC' + 'DE'
+3 * 'GH' + 3     # can't SUM int and srtring
+
+word = 'Programming'
+word[0]
+word[5]
+word[3:5]
+word[4:9]
+word[:5]
+word[8:]
+word[5:-3]
+word[:2] + word[3:]
