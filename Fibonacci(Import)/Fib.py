6a16d35d7f4714b38cae3e4acaf64cb458f358ec
SweLinHtun15 <swelinhtun15@gmail.com>
2020-01-12 20:19:25 +0630

    Fibonacci

---
+++ Fibonacci(Import)/Fib.py
@@ -0,0 +1,32 @@
+# import Fibonacci
+
+# f = Fibonacci.fib
+# f(100)
+
+# Fibonacci.fib(10000)
+#----------------------
+
+# from Fibonacci import fib, fib2
+
+# fib2(500)
+#-------------------------
+
+# f = fib2(500)
+# print(f)
+#-------------------------
+
+from Fibonacci import *
+fib(500)
+print(fib2(500))
+#--------------------------
+
+# import Fibonacci as fi
+# fi.fib(500)
+#--------------------------
+
+# from Fibonacci import fib as Fibonacci
+# Fibonacci(500)
+#---------------------------
+
+#https://docs.python.org/3/tutorial/modules.html#pa
+
